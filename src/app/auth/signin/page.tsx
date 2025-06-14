f03e1a8baf6b78f46976989989c061446e558702
9gods <codingcomelli@gmail.com>
2025-06-14 07:38:14 -0300

    feat: implements signin page

---
+++ src/app/auth/signin/page.tsx
@@ -0,0 +1,26 @@
+import { FlickeringParticles } from "@/components/animations/flickering-particles";
+import { ModeToggle } from "@/components/mode-toggle";
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col items-center justify-center w-screen h-screen">
+      <Card className="z-10 min-w-sm">
+        <CardHeader>
+          <CardTitle>Entrar no Scriptum</CardTitle>
+          <CardDescription>
+            Insira seu email e senha para acessar sua conta.
+          </CardDescription>
+        </CardHeader>
+        a
+      </Card>
+      <ModeToggle className="absolute top-4 right-4" />
+      <FlickeringParticles className="absolute inset-0 invert dark:invert-0 dark:opacity-10 opacity-20" />
+    </div>
+  );
+}
