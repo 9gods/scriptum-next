db42005ee6b8ede837d853b4806c13fc2ab0cabb
9gods <codingcomelli@gmail.com>
2025-06-22 15:03:35 -0300

    refactor: improve variable naming, export a form value type

--- src/schemas/notes-schema.ts
+++ src/schemas/notes-schema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
-export const formSchema = z.object({
-  title: z.string().min(2, { message: "Mínimo 2 caracteres" }),
-  content: z.string().min(10, { message: "Mínimo 10 caracteres" }),
-  tags: z.array(z.string().min(1)),
-  color: z.string().default('#ffffff'),
-  isPinned: z.boolean().default(false),
+export const noteSchema = z.object({
+	title: z.string().min(2, { message: "Mínimo 2 caracteres" }),
+	content: z.string().min(10, { message: "Mínimo 10 caracteres" }),
+	tags: z.array(z.string().min(1)),
+	color: z.string().default("#ffffff"),
+	isPinned: z.boolean().default(false),
 });
+
+export type NoteFormValues = z.infer<typeof noteSchema>;
